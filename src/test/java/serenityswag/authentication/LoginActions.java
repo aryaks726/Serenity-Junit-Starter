72255a033bce1009df92d4945f1e0b7836acd606
Arya <aryaks726@gmail.com>
2022-04-06 18:47:44 +0545

    practice

---
+++ src/test/java/serenityswag/authentication/LoginActions.java
@@ -0,0 +1,16 @@
+package serenityswag.authentication;
+
+import net.serenitybdd.core.steps.UIInteractionSteps;
+import net.thucydides.core.annotations.Step;
+
+public class LoginActions extends UIInteractionSteps {
+    @Step("Login as {0}")
+    public void as(User user) {
+        openUrl("https://www.saucedemo.com/");
+
+        //Login as a Standard user
+        $(LoginForm.USER_NAME).sendKeys(user.getUsername());
+        $(LoginForm.PASSWORD).sendKeys(user.getPassword());
+        $(LoginForm.LOGIN_BUTTON).click();
+    }
+}
---
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -0,0 +1,42 @@
+package serenityswag.authentication;
+
+import net.serenitybdd.core.Serenity;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import serenityswag.inventory.InventoryPage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static serenityswag.authentication.User.STANDARD_USER;
+
+@RunWith(SerenityRunner.class)
+public class WhenLoggingOn {
+
+    @Managed
+    WebDriver driver;
+
+    @Steps
+    LoginActions login;
+
+    InventoryPage inventoryPage;
+
+    @Test
+    public void usersCanLogOnViaTheHomePage() {
+//        driver.get("https://www.saucedemo.com/");
+
+        login.as(STANDARD_USER);
+//        login.as(User.LOCKED_OUT_USER);
+//        login.as(User.PROBLEM_USER);
+//        login.as(User.PERFORMANCE_GLITCH_USER);
+
+
+        //     Should see product catalog
+        Serenity.reportThat("The inventory page should be displayed with the correct title",
+                () -> assertThat(inventoryPage.getHeading()).isEqualToIgnoringCase("Products")
+        );
+    }
+
+}
